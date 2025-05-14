0cc76c560f35b4e5929a4b4bca058bbcfdad93d0
Roushan Kumar <rk04393@gmail.com>
2025-05-14 12:36:15 +0530

    Protected Route.

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,17 +2,24 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useMyContext } from "../store/ContextApi";
 
+//This ProtectedRoute component is used to protect the routes in the application.
+// It checks if the user is authenticated and if they have the required permissions to access the route.
+// Here children is the component that is being protected.
+// The component uses the useMyContext hook to access the token and isAdmin state from the ContextProvider.
+// and adminPage is a boolean that indicates if the route is an admin page.
 const ProtectedRoute = ({ children, adminPage }) => {
   // Access the token and isAdmin state by using the useMyContext hook from the ContextProvider
   const { token, isAdmin } = useMyContext();
 
   //navigate to login page to an unauthenticated
   if (!token) {
+    //Navigate means it will redirect to the login page
     return <Navigate to="/login" />;
   }
 
   //navigate to access-denied page if a user try to access the admin page
   if (token && adminPage && !isAdmin) {
+    //Navigate means it will redirect to the access-denied page
     return <Navigate to="/access-denied" />;
   }
 
