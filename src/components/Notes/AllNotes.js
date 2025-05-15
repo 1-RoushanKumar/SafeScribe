40ac1b1a86185510af3bdf2a8476dfcaef12c3c8
Roushan Kumar <rk04393@gmail.com>
2025-05-15 10:06:10 +0530

    Here we how AllNotes are loaded.

--- src/components/Notes/AllNotes.js
+++ src/components/Notes/AllNotes.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/api";
-import NoteItems from "./NoteItems";
+import NoteItems from "./NoteItems"; // Importing the NoteItems component to display individual notes if you see when we click MyNotes comes for that user each note card is called NoteItems
 import { FiFilePlus } from "react-icons/fi";
 import { Blocks } from "react-loader-spinner";
 import Errors from "../Errors";
 
+//AllNotes component is used to display all the notes created by the user.
 const AllNotes = () => {
+  //here we define the state variables using useState hook
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  //fetchNotes function is used to fetch all the notes from the backend using axios
   const fetchNotes = async () => {
     setLoading(true);
     try {
+      // Sending a GET request to the backend to fetch all notes
+      // The response is stored in the response variable.
       const response = await api.get("/notes");
 
+      // The response data is an array of notes.
+      // We are mapping through the response data and parsing the content of each note.
       const parsedNotes = response.data.map((note) => ({
-        ...note,
-        parsedContent: JSON.parse(note.content).content, // Assuming each note's content is JSON-formatted.
+        ...note, // Spread all existing properties of the note object into the new object
+        parsedContent: JSON.parse(note.content).content, // Add a new property 'parsedContent' extracted from the parsed JSON
+        //We using note.content because in our backend we are using content as the key (you can see it in postman when we are creating a note)
+        //Then you thinking why (note.content).content why two times content because in our backend we are using content as the key and in the frontend we are using content as the key so to avoid confusion we are using note.content
       }));
+
+      // Setting the parsed notes to the notes state variable.
       setNotes(parsedNotes);
     } catch (error) {
       setError(error.response.data.message);
@@ -29,6 +40,8 @@ const AllNotes = () => {
     }
   };
 
+  //useEffect hook is used to fetch all the notes when the component is mounted
+  // The empty dependency array [] means that this effect will only run once when the component is mounted.
   useEffect(() => {
     //calling the function here to fetch all notes
     fetchNotes();
@@ -39,14 +52,20 @@ const AllNotes = () => {
     return <Errors message={error} />;
   }
 
+  // The component returns a JSX element that renders the AllNotes page.
   return (
     <div className="min-h-[calc(100vh-74px)] sm:py-10 sm:px-5 px-0 py-4">
       <div className="w-[92%] mx-auto ">
+        {/* The title of the page is displayed here. */
+        /* If the loading state is false and there are notes, display the title. */}
         {!loading && notes && notes?.length > 0 && (
           <h1 className="font-montserrat  text-slate-800 sm:text-4xl text-2xl font-semibold ">
             My Notes
           </h1>
         )}
+        {/* If the loading state is true, display a loading spinner. */}
+        {/* The Blocks component is a loading spinner that is displayed while the notes are being fetched. */}
+        {/* It is a part of the react-loader-spinner library. */}
         {loading ? (
           <div className="flex  flex-col justify-center items-center  h-72">
             <span>
@@ -64,6 +83,10 @@ const AllNotes = () => {
           </div>
         ) : (
           <>
+            {/* If there are no notes, display a message to the user. */}
+            {/* The message encourages the user to create a new note. */}
+            {/* The Link component is used to navigate to the CreateNote page when the button is clicked. */}
+            {/* The button has an icon (FiFilePlus) and a label "Create New Note". */}
             {notes && notes?.length === 0 ? (
               <div className="flex flex-col items-center justify-center min-h-96  p-4">
                 <div className="text-center">
@@ -84,8 +107,16 @@ const AllNotes = () => {
                 </div>
               </div>
             ) : (
+              // If there are notes, display them in a grid layout.
+              // The grid layout is responsive and adjusts the number of columns based on the screen size.
               <>
                 <div className="pt-10 grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-y-10 gap-x-5 justify-center">
+                  {/* The notes are mapped to the NoteItems component, which displays each note. */}
+                  {/* The NoteItems component is passed the note data as props. */}
+                  {/* The key prop is used to uniquely identify each note in the list. */}
+                  {/* The id prop is passed to the NoteItems component to identify the note. */}
+                  {/* The parsedContent prop is passed to display the content of the note. */}
+                  {/* The createdAt prop is passed to display the creation date of the note. */}
                   {notes.map((item) => (
                     <NoteItems key={item.id} {...item} id={item.id} />
                   ))}
--- src/components/Notes/NoteItems.js
+++ src/components/Notes/NoteItems.js
@@ -1,17 +1,26 @@
 import { MdRemoveRedEye } from "react-icons/md";
-import Tooltip from "@mui/material/Tooltip";
+import Tooltip from "@mui/material/Tooltip"; //
 import { IconButton } from "@mui/material";
-import { truncateText } from "../../utils/truncateText";
+import { truncateText } from "../../utils/truncateText"; // Importing the truncateText function from utils
 import { Link } from "react-router-dom";
 import "react-quill/dist/quill.snow.css";
-import moment from "moment";
+import moment from "moment"; // Importing moment.js for date formatting
 
+//The is the NoteItems component that displays individual notes.
+// It takes parsedContent, id, and createdAt as props.
 const NoteItems = ({ parsedContent, id, createdAt }) => {
+  // The parsedContent is the content of the note that will be displayed.
   const formattedDate = moment(createdAt).format("D MMMM YYYY");
   return (
     <div className="sm:px-5 px-2 py-5 shadow-md bg-noteColor shadow-white rounded-lg min-h-96 max-h-96 relative overflow-hidden ">
+      {/* The parsedContent is displayed as HTML using dangerouslySetInnerHTML. */}
+      {/*The truncateText function is used to limit the length of the content displayed. */}
+      {/* The text is styled using Tailwind CSS classes. */}
       <p
         className="text-black font-customWeight ql-editor"
+        // The dangerouslySetInnerHTML attribute is used to set the inner HTML of the element.
+        // It is used to display the parsedContent as HTML.
+        // The __html property is used to set the HTML content.
         dangerouslySetInnerHTML={{ __html: truncateText(parsedContent) }}
       ></p>
       <div className="flex justify-between items-center  absolute bottom-5 sm:px-5 px-2 left-0 w-full text-slate-700">
